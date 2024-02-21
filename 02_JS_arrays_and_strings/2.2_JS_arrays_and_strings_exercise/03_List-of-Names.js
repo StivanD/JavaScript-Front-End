04da2b5b4dd4aac3e0c0193325ddf1e6d8d003ae
Stivan Dimitrov <stivandimitrov27@gmail.com>
2024-02-21 22:01:07 +0200

    Added solution

--- 02_JS_arrays_and_strings/2.2_JS_arrays_and_strings_exercise/03_List-of-Names.js
+++ 02_JS_arrays_and_strings/2.2_JS_arrays_and_strings_exercise/03_List-of-Names.js
@@ -1,5 +1,5 @@
 function solve(namesArr) {
-    namesArr.sort((a, b) => a.localeCompare(b))
+    namesArr.sort((a, b) => a.localeCompare(b));
     
     for (let i = 0; i < namesArr.length; i++) {
         console.log(`${i + 1}.${namesArr[i]}`);
