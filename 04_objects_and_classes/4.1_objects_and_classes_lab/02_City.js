89e13c60fc043c3989ea57ec0f76f773f7b52df1
Stivan Dimitrov <stivandimitrov27@gmail.com>
2024-02-25 22:21:45 +0200

    Added solution

--- 04_objects_and_classes/4.1_objects_and_classes_lab/02_City.js
+++ 04_objects_and_classes/4.1_objects_and_classes_lab/02_City.js
@@ -5,18 +5,18 @@ function city(cityObj) {
     }
 }
 
-city({
-    name: "Sofia",
-    area: 492,
-    population: 1238438,
-    country: "Bulgaria",
-    postCode: "1000"
-});
+// city({
+//     name: "Sofia",
+//     area: 492,
+//     population: 1238438,
+//     country: "Bulgaria",
+//     postCode: "1000"
+// });
 
-city({
-    name: "Plovdiv",
-    area: 389,
-    population: 1162358,
-    country: "Bulgaria",
-    postCode: "4000"
-});
+// city({
+//     name: "Plovdiv",
+//     area: 389,
+//     population: 1162358,
+//     country: "Bulgaria",
+//     postCode: "4000"
+// });
